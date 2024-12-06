687912a5f81555b9192841dc823c747628657220
ZhenyuWei <Zhenyu_Wei@sjtu.edu.cn>
2024-12-06 22:51:03 +0800

    add leap_hand

--- teleop/teleop_hand.py
+++ teleop/teleop_hand.py
@@ -178,16 +178,6 @@ class Sim:
         self.left_root_states = self.root_states[left_idx]
         self.right_root_states = self.root_states[right_idx]
 
-        x_axis_dir = np.array([0, 0, 0, 1, 0, 0], dtype=np.float32)
-        x_axis_color = np.array([1, 0, 0], dtype=np.float32)
-        self.gym.add_lines(self.viewer, env, 1, x_axis_dir, x_axis_color)
-        y_axis_dir = np.array([0, 0, 0, 0, 1, 0], dtype=np.float32)
-        y_axis_color = np.array([0, 1, 0], dtype=np.float32)
-        self.gym.add_lines(self.viewer, env, 1, y_axis_dir, y_axis_color)
-        z_axis_dir = np.array([0, 0, 0, 0, 0, 1], dtype=np.float32)
-        z_axis_color = np.array([0, 0, 1], dtype=np.float32)
-        self.gym.add_lines(self.viewer, env, 1, z_axis_dir, z_axis_color)
-
         # create default viewer
         self.viewer = self.gym.create_viewer(self.sim, gymapi.CameraProperties())
         if self.viewer is None:
@@ -201,6 +191,16 @@ class Sim:
         self.left_cam_offset = np.array([0, 0.033, 0])
         self.right_cam_offset = np.array([0, -0.033, 0])
         self.cam_pos = np.array([-0.6, 0, 1.6])
+        
+        x_axis_dir = np.array([0, 0, 0, 1, 0, 0], dtype=np.float32)
+        x_axis_color = np.array([1, 0, 0], dtype=np.float32)
+        self.gym.add_lines(self.viewer, env, 1, x_axis_dir, x_axis_color)
+        y_axis_dir = np.array([0, 0, 0, 0, 1, 0], dtype=np.float32)
+        y_axis_color = np.array([0, 1, 0], dtype=np.float32)
+        self.gym.add_lines(self.viewer, env, 1, y_axis_dir, y_axis_color)
+        z_axis_dir = np.array([0, 0, 0, 0, 0, 1], dtype=np.float32)
+        z_axis_color = np.array([0, 0, 1], dtype=np.float32)
+        self.gym.add_lines(self.viewer, env, 1, z_axis_dir, z_axis_color)
 
         # create left 1st preson viewer
         camera_props = gymapi.CameraProperties()
