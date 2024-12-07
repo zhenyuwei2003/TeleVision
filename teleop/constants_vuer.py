dd6cb0f6c1b075d7b210cc4dd46dc0515da77a83
ZhenyuWei <Zhenyu_Wei@sjtu.edu.cn>
2024-12-07 15:37:33 +0800

    add leap_hand

--- teleop/constants_vuer.py
+++ teleop/constants_vuer.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # tip_indices = [4, 9, 14, 19, 24]
-tip_indices = [4, 9, 14, 19]
+tip_indices = [4, 9, 14, 24]
 
 # hand2inspire = np.array([[0, -1, 0, 0],
 #                          [0, 0, -1, 0],
